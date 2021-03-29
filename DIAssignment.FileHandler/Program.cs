500ccb4dc83e765cc30752d12c9e8305013a494b
David Pires <developer@davidpires.pt>
2021-03-29 21:10:41 +0100

    Add basic structure

---
+++ DIAssignment.FileHandler/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace DIAssignment.FileHandler
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
